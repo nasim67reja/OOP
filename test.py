fa021ba202aa1195b41a9cf380c31ac3503d4c5d
nasim67reja <nasimreja67@gmail.com>
2025-05-20 22:09:28 +0600

    update oop ipynb

---
+++ test.py
@@ -0,0 +1,17 @@
+
+
+class Test:
+    def __init__(self):
+        pass
+
+    def print(self):
+        print("Hello, World!")
+
+    def sum(self, a, b):
+        return a + b
+
+
+
+first = Test()
+add=first.sum(2,3)
+print(add)
